ebef4ac57f9b91d8170e319e76909a5dfa9d9ff5
udonwa10 <umohu67@gmail.com>
2020-09-02 04:36:42 -0700

    get ready for heroku

--- codedaddies_list/settings.py
+++ codedaddies_list/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -125,3 +126,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
